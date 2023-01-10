68b414f8c4b82e414b6af7586db11eb40c0f7dfc
Salman Hashmi <salman@hashmi.site>
2023-01-09 18:21:38 -0800

    Implement calculator

--- calculator.js
+++ calculator.js
@@ -4,3 +4,15 @@ var m = 6;
 function add() {
     return n + m;
 }
+
+function sub() {
+    return n - m;
+}
+
+function multiply() {
+    return n * m;
+}
+
+function exponent() {
+    return n ** m;
+}
